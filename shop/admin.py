160fbc4dc940f470ce2f9842d01c473e7d70ca9e
Maktolik <maktolik97@gmail.com>
2024-06-24 16:30:38 +0300

    admin modified

--- shop/admin.py
+++ shop/admin.py
@@ -3,8 +3,19 @@ from django.contrib import admin
 from .models import Game, Genre
 
 
+admin.site.site_header = 'Makshop Admin'
+admin.site.site_title = 'Makshop'
+admin.site.index_title = 'Welcome to Makshop Admin'
+
+
 class GenreInLine(admin.TabularInline):
     model = Genre.games.through
+
+
+def description_short(obj) -> str | None:
+    if obj.description is None or len(obj.description) < 48:
+        return obj.description
+    return obj.description[:48] + "..."
 
 
 @admin.register(Game)
@@ -12,14 +23,9 @@ class GameAdmin(admin.ModelAdmin):
     inlines = [
         GenreInLine
     ]
-    list_display = 'pk', 'title', 'description_short', 'release_date', 'size', 'price', 'on_sale'
+    list_display = 'pk', 'title', description_short, 'release_date', 'size', 'price', 'on_sale'
     list_display_links = 'pk', 'title'
     ordering = 'pk', 'title'
-
-    def description_short(self, obj: Game) -> str:
-        if len(obj.description) < 48:
-            return obj.description
-        return obj.description[:48] + "..."
 
 
 class GameInline(admin.StackedInline):
@@ -32,7 +38,7 @@ class GenreAdmin(admin.ModelAdmin):
         GameInline,
     ]
 
-    list_display = 'pk', 'title'
+    list_display = 'pk', 'title', description_short
     list_display_links = 'pk', 'title'
 
 
